afc19eb19c51af48849a33e77cf7a05c3e72ee0c
Adrienne Grace <adrienne@vandepoelproductions.com>
2020-02-27 07:23:36 -0800

    basic user controller, model, route

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ require('./config/database');
 app.use(logger('dev'));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+
+app.use('/api/users', require('./routes/api/users'));
+
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
